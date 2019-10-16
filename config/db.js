a699686734fd4e23caa3ad4b3873a6d7c95ef0ff
yurikbv <yurikbvv@gmail.com>
2019-10-17 01:45:21 +0300

    started server part

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("MongoDB connected...")
+  } catch (e) {
+    console.error(e.message);
+    // Process exit with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
