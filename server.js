3351e4ff6ebb3865a8a078712ea19e5f8741c81b
yurikbv <yurikbvv@gmail.com>
2019-10-18 02:01:49 +0300

    backend: register, auth, login, token

--- server.js
+++ server.js
@@ -6,7 +6,16 @@ const app = express();
 //Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false}));
+
 app.get('/', (req, res) => res.send('API running.'));
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profiles', require('./routes/api/profiles'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
