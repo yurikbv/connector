a6d657e434b59a42752c7edddb12daf726a3d21c
yurikbv <yurikbvv@gmail.com>
2019-10-18 20:42:25 +0300

    add get my profile route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -55,7 +55,7 @@ router.post('/',[
     jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (err, token) => {
       if (err) throw err;
       res.json({token})
-    }) 
+    })
 
   } catch (e) {
     console.error(e.message);
