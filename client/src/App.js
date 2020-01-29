585c6b7c43d37bcb020ad049f9a1ab93ce2e57bd
yurikbv <yurikbvv@gmail.com>
2020-01-29 23:49:34 +0300

    added education&experience forms,actions,reducers

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,8 @@ import Dashboard from "./components/dashboard/Dashboard";
 import PrivateRoute from "./components/routing/PrivateRoute";
 import CreateProfile from "./components/profile-forms/CreateProfile";
 import EditProfile from "./components/profile-forms/EditProfile";
+import AddExperience from "./components/profile-forms/AddExperience";
+import AddEducation from "./components/profile-forms/AddEducation";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -41,6 +43,8 @@ const App = () => {
                   <PrivateRoute exact path="/dashboard" component={Dashboard}/>
                   <PrivateRoute exact path="/create-profile" component={CreateProfile}/>
                   <PrivateRoute exact path="/edit-profile" component={EditProfile}/>
+                  <PrivateRoute exact path="/add-experience" component={AddExperience}/>
+                  <PrivateRoute exact path="/add-education" component={AddEducation}/>
               </section>
             </Switch>
           </Fragment>
